1def9d83c775b5083035b28b3e525192ae93d151
ytyaru <ytyaru@outlook.jp>
2019-11-06 09:23:05 +0900

    初回コミット

---
+++ src/CodeGen/CodeNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel; // ReadOnlyCollection
+
+namespace CodeGen
+{
+    class CodeNode : CodeTree
+    {
+        public string Value { get; }
+        public CodeNode(string value) : base() => Value = value;
+        public new CodeNode Add(CodeNode node) { children.Add(node); return this; }
+    }
+}
---
+++ src/CodeGen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gen = new CodeGenerator();
+            var tree = new CodeTree()
+                .Add(new CodeNode("using System;"))
+                .Add(new CodeNode(""))
+                .Add(new CodeBlock("namespace MyNamespace")
+                    .Add(new CodeBlock("class MyClass")
+                        .Add(new CodeBlock("static void Main(string[] args)")
+                            .Add(new CodeNode(@"Console.WriteLine(""Hello world"");")))));
+            Console.WriteLine(gen.Generate(tree));
+        }
+    }
+}
